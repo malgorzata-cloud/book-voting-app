8228a3f7113feaeab34bdec237dc2faba761eabb
malgorzata-cloud <malgorzataguzel@gmail.com>
2025-09-28 13:03:26 +0200

    Fix following local host

--- server.js
+++ server.js
@@ -136,4 +136,4 @@ app.post('/vote', (req, res) => {
     res.send('<h2>Thank you for voting!</h2>');
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
